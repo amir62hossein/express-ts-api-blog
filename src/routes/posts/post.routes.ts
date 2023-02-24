076bae0a88ff50080fc313dbf8bde1ed825e0f3f
amir62hossein <amirnourian378@gmail.com>
2023-02-24 22:37:49 +0330

    upload image in post api done

--- src/routes/posts/post.routes.ts
+++ src/routes/posts/post.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import PostController from '../../controllers/post/post.controller';
+import { upload } from '../../middlewares/uploads/upload.middleware';
 const postRouter = Router();
 const postController = new PostController();
 
-postRouter.post('/', postController.createPost);
+postRouter.post('/',upload.single("photo") ,postController.createPost);
 postRouter.get('/:id', postController.getPostById);
 postRouter.get('/:page/:limit', postController.getAllPosts);
 postRouter.delete('/:id', postController.deletePost);
